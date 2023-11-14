e3ce1c48664b63cdd75bb4f2981b9e8432face13
spacebruce <noel.abercrombie@hotmail.com>
2023-11-14 09:32:58 +0000

    Quick n dirty pause

--- src/States/StateGame.c
+++ src/States/StateGame.c
@@ -2,11 +2,14 @@
 #include "State.h"
 
 #include "../Objects/Player.h"
+
+bool Paused;
 Player player;
 
 void StateGame_Joystick(u16 Joy, u16 Changed, u16 State)
 {
-
+    if(Changed & State & BUTTON_START)
+        Paused = !Paused;
 }
 void StateGame_Start()
 {
@@ -14,15 +17,22 @@ void StateGame_Start()
 }
 void StateGame_End()
 {
-    VDP_clearText(0,20,40);
     VDP_setBackgroundColor(0);
+    VDP_clearPlane(BG_B, TRUE);
 }
 
 s8 pal = 0;
 void StateGame_Tick()
 {
     VDP_setBackgroundColor(pal);
-    VDP_drawText("This is what a game looks like", 0,20);
+    VDP_drawTextBG(BG_B, "This is what a game looks like", 0,20);
+
+    if(Paused)
+    {
+        VDP_drawTextBG(BG_B, "Paused!", 10,0);
+        return;
+    }
+    
     pal = (pal + 1) % 64;
     if(pal == 0)
         STATE_NEXT = STATE_MENU;
