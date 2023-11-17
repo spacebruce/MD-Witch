fe782f2d4190348db9b15a962a6e77d64ec1f794
spacebruce <noel.abercrombie@hotmail.com>
2023-11-17 22:40:14 +0000

    Store some game values globally

---
+++ source/src/GameState.c
@@ -0,0 +1,6 @@
+#include "GameState.h"
+
+
+u8 GAME_LEVEL = 0;
+u16 GAME_FRAME = 0;
+const StageBase* GAME_LEVELPTR = NULL;
---
+++ source/src/GameState.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <genesis.h>
+
+#include "Stages/Stages.h"
+/*
+    Global stuff store
+*/
+
+extern u8 GAME_LEVEL;
+extern u16 GAME_FRAME;
+extern const StageBase* GAME_LEVELPTR;
