98cd785b6700252232d43d563005fa5a922cde1d
spacebruce <noel.abercrombie@hotmail.com>
2024-03-17 14:09:53 +0000

    Throw out level editor files

--- Assets/Levels/tilesheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tilesheet" tilewidth="16" tileheight="16" tilecount="7" columns="7">
- <image source="../../source/res/tiles/tilesheet.png" width="112" height="16"/>
-</tileset>
